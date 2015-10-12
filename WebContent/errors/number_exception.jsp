c7a0e2988fe434aa1bac8b95b29ab7c3cef96e1d
Oleg Dashkevych <oleg.daskevych@gmail.com>
2015-10-12 21:00:20 +0300

    Added error pages

---
+++ WebContent/errors/number_exception.jsp
@@ -0,0 +1,12 @@
+<%@ page isErrorPage="true" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<html>
+<head>
+<title>Error Page</title>
+</head>
+<link rel="STYLESHEET" type="text/css" href="./decoration.css">
+<body>
+	<h1>Input Error</h1>
+	<div class="result">Invalid numbers!</div>
+</body>
+</html>
--- src/ua/nure/dashkevych/Task1.java
+++ src/ua/nure/dashkevych/Task1.java
@@ -18,24 +18,11 @@ public class Task1 extends HttpServlet {
 		Calculator calculator = new Calculator();
 		Double first = null;
 		Double second = null;
-		try {
-			first = Double.valueOf(req.getParameter("first").toString());
-			second = Double.valueOf(req.getParameter("second").toString());
-		} catch (NumberFormatException e) {
-			req.getSession().setAttribute("result", "");
-			req.getSession().setAttribute("result", e.getMessage());
-		}
-		String operation = req.getParameter("operation").toString();
-		try {
-			Double result = calculator.doOperation(operation, first, second);
-			req.setAttribute("result", result);
-		} catch (IllegalArgumentException e) {
-			req.setAttribute("result", e.getMessage());
-		}
-		finally {
-			req.getRequestDispatcher("result.jsp").forward(req, resp);
-			
-		}
+		first = Double.valueOf(req.getParameter("first"));
+		second = Double.valueOf(req.getParameter("second"));
+		String operation = req.getParameter("operation");
+		Double result = calculator.doOperation(operation, first, second);
+		req.setAttribute("result", result);
+		req.getRequestDispatcher("result.jsp").forward(req, resp);
 	}
-
 }
